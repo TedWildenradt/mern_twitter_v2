f98e0a054b87dae520f4c68d10ead133741743b2
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-26 12:33:12 -0800

    Tweet route setup

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Tweet = require('../../models/Tweet');
+const validateTweetInput = require('../../validation/tweets');
 
 router.get("/test", (req, res) => res.json({msg: "This is the tweets route"}))
 
